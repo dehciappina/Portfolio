058e49da9a63f6cc907edbcd4750db33d3aed74f
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2021-01-05 11:30:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,5 @@
-const hamburguer = document.querySelector('#hamburguer');
-const hamburguerDivs = document.querySelectorAll('#hamburguer div div:not(#fixed_dot)');
-const nav = document.querySelector('nav');
-const navBts = document.querySelectorAll('.contact_bt');
-const fixedDot = document.querySelector('#fixed_dot')
-const navBtsSVG = document.querySelectorAll('.contact_bt svg');
-
-let showingContacts = false;
-
-hamburguer.addEventListener('click', function() {
-    toggleContacts()
-})
-
-function toggleContacts() {
-    if(showingContacts == false) {
-        showingContacts = true;
-        nav.classList.toggle('pressed_nav')
-
-        for(i=0; i<navBts.length; i++ ) {
-            navBts[i].style.opacity = 1;
-            navBts[i].style.transform = 'scale(1)';
-        }
-
-        for(i=0; i<hamburguerDivs.length; i++ ) {
-            hamburguerDivs[i].style.transform = 'translateX(-4rem) scale(0)';
-        }
-        setTimeout(() => {
-            fixedDot.style.transform = 'scale(2)'
-        }, 400);
-
-    } else {
-        showingContacts = false;
-        nav.classList.toggle('pressed_nav')
-
-        for(i=0; i<navBts.length; i++ ) {
-            navBts[i].style.opacity = 0;
-            navBts[i].style.transform = 'scale(0.9)';
-        }
-
-        fixedDot.style.transform = 'scale(1)'
-
-        for(i=0; i<hamburguerDivs.length; i++ ) {
-            hamburguerDivs[i].style.transform = 'translateX(0rem) scale(1)';
-        }
-
-    }
-}
+AOS.init({
+    duration: 1000,
+    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+    offset: 120,
+});
