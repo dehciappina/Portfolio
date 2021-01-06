ae4731d07e3f84fe48e89785746f50ee5914661c
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2021-01-06 14:54:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-AOS.init({
+
+
+ScrollReveal().reveal('.post', {
+    distance: '30px',
+    duration: 1400,
+    reset: true,
+    easing: 'ease',
+    elements: {},
+    viewOffset: {
+        bottom: window.innerHeight/1.8,
+    },
+})
+
+
+ScrollReveal().reveal('.post_line', {
+    distance: window.innerWidth * 0.1 + 'px',
     duration: 1000,
-    easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
-    offset: 120,
-});
+    reset: true,
+    easing: 'ease',
+    elements: {},
+    origin: 'left',
+    viewOffset: {
+        bottom: window.innerHeight/4,
+    },
+})
